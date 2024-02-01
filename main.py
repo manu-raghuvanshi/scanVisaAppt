31bee288a98d2aa7692c653c7cc9f20a277b6fe0
manu-raghuvanshi <manu.raghuvanshi@Manus-Air.lan>
2024-02-01 15:10:05 -0500

    First working iteration

--- main.py
+++ main.py
@@ -9,14 +9,11 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 import os
-from pathlib import Path
 from dotenv import load_dotenv
 
-dotenv_path = Path("variable.env")
-load_dotenv(dotenv_path=dotenv_path)
+load_dotenv()
 
 while True:
-    time.sleep(180)
     print(time.strftime("%H:%M:%S", time.localtime()))
     # instantiate webdriver
     chrome_driver_path = os.getenv("chrome_driver_path")
@@ -41,21 +38,30 @@ while True:
     for tr in soup.find_all("tr"):
         if "data-index" in tr.attrs:
             if tr["data-index"] == "9":
-                if tr.contents[1].get_text() == "B1/B2 (Regular)" and tr.contents[0].get_text() == "NEW DELHI VAC":
-                    if ("Jan" in tr.contents[2].get_text() or "Feb" in tr.contents[2].get_text()
-                            or "Mar" in tr.contents[2].get_text() or "Apr" in tr.contents[2].get_text()):
+                if (
+                    tr.contents[1].get_text() == "B1/B2 (Regular)"
+                    and tr.contents[0].get_text() == "NEW DELHI VAC"
+                ):
+                    if (
+                        "Jan" in tr.contents[2].get_text()
+                        or "Feb" in tr.contents[2].get_text()
+                        or "Mar" in tr.contents[2].get_text()
+                        or "Apr" in tr.contents[2].get_text()
+                    ):
                         message = {
                             "location": tr.contents[0].get_text(),
                             "visa_type": tr.contents[1].get_text(),
                             "earliest_date_available": tr.contents[2].get_text(),
-                            "last_checked": tr.contents[5].get_text()
+                            "last_checked": tr.contents[5].get_text(),
                         }
                         print(message)
                         subject = "Visa Slot Alert"
                         email_sender = "manu.raghuvanshi92@gmail.com"
                         email_password = os.getenv("check_visa_slot_password")
                         email_receivers = [
-                            ]
+                            "manu.raghuvanshi92@gmail.com",
+                            "heerakchugh@gmail.com",
+                        ]
                         for email_receiver in email_receivers:
                             email_obj = EmailMessage()
                             email_obj["From"] = email_sender
@@ -65,8 +71,14 @@ while True:
 
                             ssl_context = ssl.create_default_context()
 
-                            with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=ssl_context) as smtp:
+                            with smtplib.SMTP_SSL(
+                                "smtp.gmail.com", 465, context=ssl_context
+                            ) as smtp:
                                 smtp.login(email_sender, email_password)
-                                smtp.sendmail(email_sender, email_receiver, email_obj.as_string())
+                                smtp.sendmail(
+                                    email_sender, email_receiver, email_obj.as_string()
+                                )
 
                         break
+
+    time.sleep(120)
